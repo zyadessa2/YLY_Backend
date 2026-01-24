aa5d9b70b1ac79180256f485386e52d918c4e46e
ziad <helmyzez@gmail.com>
2026-01-24 18:58:32 +0200

    update dumy routes

--- dist/modules/routes.js
+++ dist/modules/routes.js
@@ -9,11 +9,15 @@ const user_routes_js_1 = __importDefault(require("./userModule/user.routes.js"))
 const news_routes_js_1 = __importDefault(require("./newsModule/news.routes.js"));
 const event_routes_js_1 = __importDefault(require("./eventModules/event.routes.js"));
 const governorate_routes_js_1 = __importDefault(require("./governorateModule/governorate.routes.js"));
+const index_js_1 = __importDefault(require("../index.js"));
 const router = (0, express_1.Router)();
 router.use('/auth', auth_routes_js_1.default);
 router.use('/user', user_routes_js_1.default);
 router.use('/news', news_routes_js_1.default);
 router.use('/events', event_routes_js_1.default);
 router.use('/governorates', governorate_routes_js_1.default);
+index_js_1.default.all('/api/v1/*', (req, res) => {
+    res.status(404).json({ message: "API route not found" });
+});
 exports.default = router;
 //# sourceMappingURL=routes.js.map
--- src/modules/routes.ts
+++ src/modules/routes.ts
@@ -4,6 +4,7 @@ import userRouter from './userModule/user.routes.js';
 import newsRouter from './newsModule/news.routes.js';
 import eventRouter from './eventModules/event.routes.js';
 import governorateRouter from './governorateModule/governorate.routes.js';
+import app from '../index.js';
 
 
 const router = Router();
@@ -14,7 +15,9 @@ router.use('/news', newsRouter);
 router.use('/events', eventRouter);
 router.use('/governorates', governorateRouter);
 
-
+app.all('/api/v1/*', (req, res) => {
+  res.status(404).json({ message: "API route not found" });
+});
 
 
 export default router;
