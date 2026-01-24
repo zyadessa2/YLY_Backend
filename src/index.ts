b02d156935541b185b3545d978495db66356599d
ziad <helmyzez@gmail.com>
2026-01-24 18:05:40 +0200

    tst

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { app } from "./bootstrap";
 
-if (!process.env.VERCEL) {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-        console.log("server is running on port", port);
-    });
-}
+// if (!process.env.VERCEL) {
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => {
+//         console.log("server is running on port", port);
+//     });
+// }
 
 export default app;
